b598a5a879449ec067400d6a164539627379f5e3
Marek Tutaj <mtutaj@mcw.edu>
2018-10-15 11:45:12 -0500

    refactored build.gradle to produce lean runnable jar

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # go-rat-annotation-pipeline
-Imports GO annotation for rat from EBI
+Imports GO annotations for rat from EBI
+
+Source files:
+ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/RAT/goa_rat.gaf.gz
+ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/RAT/goa_rat_complex.gaf.gz
+ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/RAT/goa_rat_isoform.gaf.gz
+ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/RAT/goa_rat_rna.gaf.gz
+
--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -10,8 +10,7 @@ APPNAME=GOAannotation
 APPDIR=/home/rgddata/pipelines/$APPNAME
 
 cd $APPDIR
-pwd
-DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
-LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
-export GO_AANNOTATION_OPTS="$DB_OPTS $LOG4J_OPTS"
-bin/$APPNAME "$@"
+
+java -Dspring.config=$APPDIR/../properties/default_db.xml \
+  -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+  -jar ./$APPNAME.jar "$@"
